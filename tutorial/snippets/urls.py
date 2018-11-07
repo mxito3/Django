77bc8c197796a8c4cdf0913f9d1ab5ff7d1cdadd
mxito3 <1477311729@qq.com>
2018-11-07 14:03:27 +0800

    添加api schema　以及core-api -cli用法

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from snippets import views
+from rest_framework.schemas import get_schema_view
+
+
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
@@ -10,4 +13,9 @@ router.register(r'users', views.UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
+]
+
+schema_view = get_schema_view(title='Pastebin API')
+urlpatterns += [
+    path('schema/', schema_view),
 ]
