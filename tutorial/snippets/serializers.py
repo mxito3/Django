e8750c2df36f01984626d02eea921bb775d39a8f
mxito3 <1477311729@qq.com>
2018-11-07 12:47:18 +0800

    给snippets添加超链接 高亮

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -3,22 +3,24 @@ from snippets.models import Snippet
 from django.contrib.auth.models import User
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     # owner字段必须要有，因为Snippet里面没有owner字段
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
         # fields里面是需要序列化的字段，得把owner加进去
-        fields = ('id', 'title', 'code', 'linenos',
-                  'language', 'style', 'owner')
+        fields = ('url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
 
 
 class UserSerializer(serializers.ModelSerializer):
         # snippets指的是user创建的snippets
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all())
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'id', 'username', 'snippets')
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,6 +4,11 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from rest_framework import renderers
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -12,7 +17,8 @@ class SnippetList(generics.ListCreateAPIView):
     # permission_classes用来限制权限
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
-    #存入模型前存入user
+    # 存入模型前存入user
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
@@ -32,3 +38,23 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        # 返回的是用用户建立snippet实例时选择的render方式渲染的html(序列化后)
+
+        return Response(snippet.highlighted)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+        # reverse返回完全编码的url格式，第一个参数是url name
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
