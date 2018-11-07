6df2e26a526a38d980d675b16175dda20f69df43
mxito3 <1477311729@qq.com>
2018-11-07 10:27:57 +0800

    给api 使用权限

--- tutorial/manage.py
+++ tutorial/manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env ppython manage.py makemigrations snippetsython
 import os
 import sys
 
--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from snippets.models import Snippet
+from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    # owner字段必须要有，因为Snippet里面没有owner字段
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        # fields里面是需要序列化的字段，得把owner加进去
+        fields = ('id', 'title', 'code', 'linenos',
+                  'language', 'style', 'owner')
+
+
+class UserSerializer(serializers.ModelSerializer):
+        # snippets指的是user创建的snippets
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
