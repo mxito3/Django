cd4024d849c9559add39497109de1bf8a93a6c83
mxito3 <1477311729@qq.com>
2019-06-27 21:17:16 +0800

    update

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
-
+STATIC_URL='/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+print(STATIC_ROOT)
 # 分页
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
